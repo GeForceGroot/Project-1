e759a4207010eebb32eac4ee25a222e1340170e3
GeForceGroot <tiwarishreyansh007@gmail.com>
2024-04-02 17:37:49 +0530

    impact

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import AcademicImpact from './components/AcademicImpact';
 import AssigementProvideType from './components/AssigementProvideType';
 import Benifits from './components/Benifits';
 import Carousel from './components/Carousel';
+import Feedback from './components/Feedback';
 import Navbar from './components/Navbar';
 import Writer from './components/Writer';
 
@@ -13,6 +15,8 @@ function App() {
     <Benifits/>
     <Writer/>
     <AssigementProvideType/>
+    <Feedback/>
+    <AcademicImpact/>
     </>
   );
 }
---
+++ src/components/AcademicImpact.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./AcademicImpactStyle.css"
+
+const AcademicImpact = () => {
+    return (
+        <>
+            <section className='hell' style={{ justifyContent: "center", textAlign: 'center', marginTop: '50px', }}>
+                <div className='ok' style={{ display: 'flex', justifyContent: 'center' }}>
+                    <h1 style={{ padding: '7px', color: 'rgb(170 51 170 / 82%)' }}>Academic Impact</h1>
+                    <img src="https://cdn-icons-png.flaticon.com/128/3281/3281323.png" width={'50px'} height={'50px'} style={{ marginRight: '25px' }} alt="" />
+                </div>
+                <section className='hello' style={{display:'flex'}} >
+                    <section className="ball" style={{ marginLeft: '400px' }}>
+                        <p style={{ marginTop: '50px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>1571</p>
+                        <p style={{ marginBottom: '25px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Projects Completed</p>
+                        <div className="shadow" style={{ marginTop: '50px', marginLeft: '25px' }}></div>
+                    </section>
+                    <section className="ball" style={{ marginLeft: '300px', marginRight: '300px' }}>
+                        <p style={{ marginTop: '50px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>1042</p>
+                        <p style={{ marginBottom: '25px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Happy Students</p>
+                        <div className="shadow" style={{ marginTop: '50px', marginLeft: '25px' }}></div>
+                    </section>
+                    <section className="ball" style={{ marginRight: '400px' }}>
+                        <p style={{ marginTop: '50px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>15</p>
+                        <p style={{ marginBottom: '25px', fontSize: "15px", fontWeight: 'bold', color: "rgb(115 10 115 / 89%)" }}>Years Experience</p>
+                        <div className="shadow" style={{ marginTop: '50px', marginLeft: '25px' }}></div>
+                    </section>
+                </section>
+            </section>
+        </>
+    )
+}
+
+export default AcademicImpact
