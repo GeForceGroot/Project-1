2d40f6b85dc4b7814f93fe87db6c5c196c196297
GeForceGroot <tiwarishreyansh007@gmail.com>
2024-04-02 19:04:58 +0530

    impact 25

--- src/components/AssigementProvideType.js
+++ src/components/AssigementProvideType.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import './style.css'
+// import './style.css'
+import './AssigementProvideType.css'
 import { useState } from 'react'
 
 const AssigementProvideType = () => {
