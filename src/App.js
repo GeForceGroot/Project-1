5e950ca041990ca95d00909b5824aa0df3c00120
GeForceGroot <tiwarishreyansh007@gmail.com>
2024-04-02 16:44:53 +0530

    benifits

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Benifits from './components/Benifits';
 import Carousel from './components/Carousel';
 import Navbar from './components/Navbar';
 
@@ -7,6 +8,7 @@ function App() {
     <>
     <Navbar/>
     <Carousel/>
+    <Benifits/>
     </>
   );
 }
